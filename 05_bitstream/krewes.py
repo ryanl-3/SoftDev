ba88880fc3dd9bb63631f711b74e49440179ab10
Ryan Lee <rlee30@stuy.edu>
2022-09-29 21:12:15 -0400

    added files

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,56 @@
+from random import randint
+import re
+
+krewes1 = {1:["1", "2", "3"], 2:["A", "B", "C"]}
+krewes2 = {
+    2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"],
+    7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+    8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
+}
+krewes3 = {}
+krewes4 = {1:["1"], 2:[]}
+krewes5 = {1:[], 2:[]}
+
+file = open("krewes.txt")
+text = file.read()
+
+pd = []
+devo = []
+ducky = []
+
+diction = {}
+
+def categorize(s):
+    individual = s.split("@@@")
+    for e in individual:
+        t = e.split("$$$")
+        pd = t[0]
+        devo = t[1]
+        ducky = t[2]
+    for p in range(0, len(pd) - 1)
+        diction.add(pd[p], devo[p] + ducky[p])
+
+def getRandom(dictionary):
+    
+    if len(dictionary) == 0:
+        return "ERROR: Dictionary has no values"       
+    key = []
+    for e in dictionary.keys(): #gets all the keys
+        key.append(e)
+    while True:
+        if len(key) == 0:
+            return "ERROR: all lists empty"
+        period = key[randint(0, len(key)-1)]  #chooses a random key
+        if len(dictionary.get(period)) == 0:
+            key.remove(period)
+        else:
+            r = randint(0, len(dictionary.get(period))-1) #chooses random index of the list associated with the random key
+            k = dictionary.get(period)  #gets the list associated with the random key
+            break
+    return k[r] #returns value at random index of random list
+
+print(getRandom(krewes1))
+print(getRandom(krewes2))
+print(getRandom(krewes3))
+print(getRandom(krewes4))
+print(getRandom(krewes5))
